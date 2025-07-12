366ebfd87cafdce78d5b1af7e0e9b7a050a16fcf
erdypwr <erdyberau@gmail.com>
2025-07-12 15:11:30 +0800

    mantapk

---
+++ PyroUbot/core/helpers/msg.py
@@ -0,0 +1,7 @@
+# Placeholder for MSG object/class
+# Silakan lengkapi sesuai kebutuhan modul expired.py
+
+class MSG:
+    @staticmethod
+    def EXP_MSG_UBOT(X):
+        return f"Userbot {getattr(X.me, 'id', 'Unknown')} expired."
